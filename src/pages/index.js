3e354857169b2e98e31860af458b4b68e66a3f04
Matthieu Oger <m.oger@theexplorers.com>
2022-07-27 18:58:07 +0200

    Remove anonymous component for Index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import React from 'react'
 // Page.
 // -------------------------------------------------------------
 
-export default () => {
+const Index = () => {
   return (
     <main role="main">
       <h1>Title</h1>
-      <p>Body</p>
+      <p>Machin</p>
     </main>
   )
 }
+
+export default Index
