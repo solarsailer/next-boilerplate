f71b5d1fda36e76436630cc1e5e2ef7f0d173dc6
Matthieu Oger <m.oger@theexplorers.com>
2023-02-08 12:04:48 +0100

    Add optional script to configure TypeScript

---
+++ enable-ts.mjs
@@ -0,0 +1,46 @@
+import {join} from 'node:path'
+import {writeFileSync} from 'node:fs'
+import {execSync} from 'node:child_process'
+
+// -------------------------------------------------------------
+// Config.
+// -------------------------------------------------------------
+
+const config = `{
+  "compilerOptions": {
+    "target": "es5",
+    "lib": ["dom", "dom.iterable", "esnext"],
+    "allowJs": true,
+    "skipLibCheck": true,
+    "strict": true,
+    "forceConsistentCasingInFileNames": true,
+    "noEmit": true,
+    "esModuleInterop": true,
+    "module": "esnext",
+    "moduleResolution": "node",
+    "resolveJsonModule": true,
+    "isolatedModules": true,
+    "jsx": "preserve",
+    "incremental": true,
+    "baseUrl": ".",
+    "paths": {
+      "@/*": ["./src/*"]
+    }
+  },
+  "include": ["next-env.d.ts", "**/*.ts", "**/*.tsx"],
+  "exclude": ["node_modules"]
+}`
+
+// -------------------------------------------------------------
+// Script.
+// -------------------------------------------------------------
+
+try {
+  console.info('Enabling TypeScript…')
+  writeFileSync(join(process.cwd(), 'tsconfig.json'), config)
+  execSync('npm install @types/node @types/react @types/react-dom typescript')
+  console.info('TypeScript successfully configured for this project!')
+} catch (e) {
+  console.error('TypeScript configuration failed.')
+  console.error(e)
+}
